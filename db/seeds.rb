ae1b114d357e8f3bd9d9ccf8a3ed20795dd1a0e9
EllieRamanlal <ellie.y.ramanlal@gmail.com>
2021-08-20 19:38:55 +1000

    adds listing categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = [
+    {name: "Foliage plants", description: "Decorative, leafy plants"},
+    {name: "Flowering plants", description: "Bright, colourful blooming plants"},
+    {name: "Succulents", description: "Low-maintenance, water-storing plants"},
+    {name: "Cacti", description: "Unique-shaped, low-maintenance plants"},
+    {name: "Ferns", description: "Plants with lush fronds"},
+    {name: "Trailing plants", description: "Plants with cascading foliage"}
+]
+
+if User.count == 0 
+    User.create(username: "Tester", email: "test@test.com", password: "password", password_confirmation: "password")
+end
+
+if Category.count == 0 
+    categories.each do |category| 
+        Category.create(name: category[:name], description: category[:description])
+        puts "created #{category[:name]} category"
+    end 
+end
