23700b1cdb7181df7f1e55e5ec51dd084e664a71
EllieRamanlal <ellie.y.ramanlal@gmail.com>
2021-08-22 07:14:56 +1000

    adds styling for showing listings

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,7 @@
 module ListingsHelper
+    def image_select(listing)
+        return listing.picture if listing.picture.attached?
+        # use default image when no image is attached
+        return "default-plant.jpg"
+    end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :listings_features
-  has_many :features, through: :listings_features
+  has_many :features, through: :listings_features, dependent: :destroy
   accepts_nested_attributes_for :listings_features
   has_one_attached :picture
 end
