dbda5a3a218456d2e960fca0abd86c3429d9682a
EllieRamanlal <ellie.y.ramanlal@gmail.com>
2021-08-22 15:17:23 +1000

    sets up order model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :listings
+  has_many :sold_orders, class_name: "Order", foreign_key: "seller_id"
+  has_many :bought_orders, class_name: "Order", foreign_key: "buyer_id"
 end
