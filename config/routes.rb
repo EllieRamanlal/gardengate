30d19811216a0ce534c958c36e201ec1ffe7bcfb
EllieRamanlal <ellie.y.ramanlal@gmail.com>
2021-08-22 13:34:04 +1000

    fixes stripe

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get '/success', to: "payments#success", as: "payment_success"
   post '/payments/webhook', to: "payments#webhook", as: "webhook"
+  post 'payments', to: "payments#create_payment_intent", as: "create_payment_intent"
+
 end
